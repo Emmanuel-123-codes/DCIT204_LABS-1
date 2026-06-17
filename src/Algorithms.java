6ff1e7311dd1d26dfa8ca7f9e400cfb6425bc83b
Emmanuel-123-codes <eakacquah001@st.ug.edu.gh>
2026-06-17 13:37:58 +0100

    Implemented Linear Search and Binary Search

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,34 @@
-public class Algorithms {
+public class Algorithm {
+    public static int binarySearch(int[] array, int target){
+        int start = 0;
+        int end = array.length-1;
+        int mid = (start+end)/2;
+
+
+
+        while(start<=end){
+            System.out.println("Mid:"+array[mid]);
+            if(array[mid]==target){
+                return mid;
+            }else if(array[mid]< target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+            mid = (start+end)/2;
+        }
+        return -1;
+    }
+
+    public static int linearSearch(int [] array, int target){
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == target){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+//
+
+        // User inputs the size of the inputs represented n.
+
+        System.out.println("Enter the value of n: ");
+        int n = input.nextInt();
+
+        // An array of elements n is created and the user inputs the individual elements.
+        int [] arr = new int[n];
+        System.out.println("Enter " + n + " values: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            arr[i] = input.nextInt();
+        }
+
+        // User enters the key or what value is to e searched for.
+        System.out.println("Enter target value: ");
+        int target = input.nextInt();
+
+        // Array is sorted suppose user enters the elements unordered.
+        Arrays.sort(arr);
+
+
+        // Results from the linear and binary search are assigned to results and resultsBL respectively
+
+        int results = Algorithm.linearSearch(arr, target);
+        int resultsBL = Algorithm.binarySearch(arr, target);
+
+
+
+        // Print results.
+
+        System.out.println("Linear Search results:");
+        if (results!= -1){
+            System.out.println("Target found at index "+results);
+        } else {
+            System.out.println("TARGET_NOT_FOUND !");
+        }
+
+
+
+        System.out.println("Binary Search results:");
+        if (results!= -1){
+            System.out.println("Target found at index "+results);
+        } else {
+            System.out.println("TARGET_NOT_FOUND !");
+        }
 
     }
 }
